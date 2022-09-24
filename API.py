f6aeea4c1e254722e8a650ee7118ba9f70da4990
nivyosef12 <nivyosef12@gmail.com>
2022-09-24 15:21:57 +0300

    basic features added to base.html

--- API.py
+++ API.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form, status
+from starlette.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
 import uvicorn
 
@@ -9,6 +10,13 @@ app = FastAPI()
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse("base.html", {"request": request})
+
+
+@app.post("/search")
+def search(request: Request, title: str = Form(...)):
+    print(title)
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_303_SEE_OTHER)  # changing from post route to get route
 
 
 uvicorn.run(app, host="127.0.0.1", port=8000)
