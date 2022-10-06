69206b7a4bef49c4fb9e7d518e2e5850239d74c2
nivyosef12 <nivyosef12@gmail.com>
2022-10-06 12:50:34 +0300

    deleted unneccesery files

--- routes/search_results_route.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-from config.database import client
-from models.search_results_model import SearchResult
-from schemas.search_results_schema import search_result_serializer, search_results_serializer
-
-api_router = APIRouter()
